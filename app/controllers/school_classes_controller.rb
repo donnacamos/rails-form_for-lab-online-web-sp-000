9fae06cdc4afea5d74fd7aca54d0766f36cd8ed4
Donna Amos <donnacamos.developer@gmail.com>
2019-08-17 19:52:35 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,6 +10,12 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(post_params)
     @school_class.save
-
+    redirect_to school_class_path(@school_class)
   end
+
+  private 
+
+  def post_params
+    params.require(:school_class).permit(:title, :room_number)
+  end 
 end
