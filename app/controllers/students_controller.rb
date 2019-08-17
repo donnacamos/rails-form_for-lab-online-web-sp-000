9a27e66171f1c9f7d5b6c62b7eb6379630ecc8f2
Donna Amos <donnacamos.developer@gmail.com>
2019-08-17 19:58:00 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,5 +10,17 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(post_params)
     @student.save
+    redirect_to student_path(@student)
   end
+
+  def show 
+    @student = Student.find(params[:id])
+  end 
+
+  def edit
+    @student = Student.find(params[:id])
+  end 
+
+  def update
+    @student = Student.find(params[:id]) 
 end 
