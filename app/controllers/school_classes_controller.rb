7498a56a4604483a8e38e3fefeea994c38d1ddee
Donna Amos <donnacamos.developer@gmail.com>
2019-08-17 19:50:49 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,15 +1,15 @@
 class SchoolClassesController < ApplicationController
-  def index 
-    @school_classes = SchoolClass.all 
-  end 
+  def index
+    @school_classes = SchoolClass.all
+  end
 
-  def new 
+  def new
     @school_class = SchoolClass.new
-  end 
+  end
 
   def create
     @school_class = SchoolClass.new(post_params)
     @school_class.save
-    
-  end 
+
+  end
 end
